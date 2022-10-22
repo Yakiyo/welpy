29cbc04b008a867b64c5d7e94abac45e69db4416
Yakiyo <91675932+Yakiyo@users.noreply.github.com>
2022-10-22 21:54:33 +0600

    fix: handle empty avatars
    - stupid discord returns `None` when default avatar
    - handle error for it
    - use a hardcoded link for a default image

--- fetch.py
+++ fetch.py
@@ -10,9 +10,14 @@ def fetch(id):
 		'Authorization': f'Bot {getenv("DISCORD_TOKEN")}'
 	  }
 	res = get(f'https://discord.com/api/users/{id}', headers=heads).json()
-	if 'message' in res and res['message'] == 'Unknown User':
+	if 'message' in res:
 		return None
-	return {
+
+	user = {
 		'name': f'{res["username"]}#{res["discriminator"]}',
 		'avatar': f'https://cdn.discordapp.com/avatars/{res["id"]}/{res["avatar"]}.png?size=1024'
 	}
+	if res['avatar'] is None:
+		user['avatar'] = 'https://cdn.discordapp.com/embed/avatars/4.png'
+
+	return user
